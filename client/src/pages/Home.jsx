ba045acd40a219d2d735a966a9acb9d5b5765b50
Manish <manish2003sharma@gmail.com>
2025-10-01 21:27:07 +0530

    pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SyncedGrid from '../components/SyncedGrid';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>🔄 Synced React Grid Layout</h1>
+            <SyncedGrid />
+        </div>
+    );
+}
+
+export default Home;
