57c45895a2de2f1a550232387e5d966bf8e73447
Manish <manish2003sharma@gmail.com>
2025-10-01 22:14:54 +0530

    api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/layouts";
+const API_URL = "https://cardtree1-2.onrender.com/layouts";
 
 export const getLayouts = async () => {
     const res = await axios.get(API_URL);
